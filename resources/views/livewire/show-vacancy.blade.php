2064427936377000e81ea95c4a68b53e47314c07
David Martínez Aliaga <158084789+DavidMartinezo039@users.noreply.github.com>
2025-03-18 20:13:37 +0100

    No mostrar el subir cv si no te autenticaste y cambiar ruta del logo

--- resources/views/livewire/show-vacancy.blade.php
+++ resources/views/livewire/show-vacancy.blade.php
@@ -46,7 +46,9 @@
         </div>
     @endguest
 
-    @cannot('create', \App\Models\Vacancy::class)
-        <livewire:apply-vacancy :vacancy="$vacancy"/>
-    @endcannot
+    @auth
+        @cannot('create', \App\Models\Vacancy::class)
+            <livewire:apply-vacancy :vacancy="$vacancy"/>
+        @endcannot
+    @endauth
 </div>

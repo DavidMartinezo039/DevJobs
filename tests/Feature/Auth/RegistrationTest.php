6ec7d7fc5cfa962737d70462d24fbc2c1257ff7b
David Martínez Aliaga <158084789+DavidMartinezo039@users.noreply.github.com>
2025-05-06 13:08:13 +0200

    Arreglos de rutas

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -25,5 +25,5 @@ test('new users can register', function () {
     $this->actingAs($user);
 
     $this->assertAuthenticated();
-    $response->assertRedirect(route('vacancies.index', absolute: false));
+    $response->assertRedirect(route('vacancies.manager', absolute: false));
 });

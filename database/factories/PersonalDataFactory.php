f14a243fd8f10566203b6dade115e97e86011cae
David Martínez Aliaga <158084789+DavidMartinezo039@users.noreply.github.com>
2025-06-09 16:01:59 +0200

    arreglar factories

--- database/factories/PersonalDataFactory.php
+++ database/factories/PersonalDataFactory.php
@@ -6,6 +6,8 @@ use App\Models\CV;
 use App\Models\Gender;
 use App\Models\PersonalData;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class PersonalDataFactory extends Factory
 {
@@ -13,6 +15,11 @@ class PersonalDataFactory extends Factory
 
     public function definition(): array
     {
+        $newImageName = 'profile_' . Str::random(10) . '.png';
+
+        if (!Storage::disk('public')->exists('images/' . $newImageName)) {
+            Storage::disk('public')->copy('images/default/default.png', 'vacancies/' . $newImageName);
+        }
         return [
             'cv_id' => Cv::factory(),
             'first_name' => $this->faker->firstName,
--- database/factories/VacancyFactory.php
+++ database/factories/VacancyFactory.php
@@ -7,6 +7,8 @@ use App\Models\Salary;
 use App\Models\User;
 use App\Models\Vacancy;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class VacancyFactory extends Factory
 {
@@ -14,12 +16,18 @@ class VacancyFactory extends Factory
 
     public function definition(): array
     {
+        $newImageName = 'vacancy_' . Str::random(10) . '.png';
+
+        if (!Storage::disk('public')->exists('vacancies/' . $newImageName)) {
+            Storage::disk('public')->copy('vacancies/default/default.png', 'vacancies/' . $newImageName);
+        }
+
         return [
             'title' => $this->faker->jobTitle(),
             'description' => $this->faker->paragraphs(3, true),
             'company' => $this->faker->company(),
             'last_day' => $this->faker->dateTimeBetween('now', '+1 month')->format('Y-m-d'),
-            'image' => 'default/default.png',
+            'image' => $newImageName,
             'salary_id' => Salary::inRandomOrder()->first()->id ?? Salary::factory(),
             'category_id' => Category::inRandomOrder()->first()->id ?? Category::factory(),
             'user_id' => User::inRandomOrder()->first()->id ?? User::factory(),
